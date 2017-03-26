3e04359b3f033a776cc8292a26df49e2327e8dce
Sreeram Goalla <shriram.goalla@gmail.com>
2017-03-26 19:24:50 +1100

    Add User Avatar to the Users Table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   has_many :articles
+  mount_uploader :avatar, AvatarUploader
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name, :last_name, :date_of_birth, :email, presence: true
 end
